18de57c6b04d53d52ba8623100ef12b82a934ec6
Letannguyen <letannguyen210496@gmail.com>
2024-05-22 00:11:54 +0700

    fragments

--- Admin/src/main/java/com/ecommerce/admin/service/AuthService.java
+++ Admin/src/main/java/com/ecommerce/admin/service/AuthService.java
@@ -1,13 +1,9 @@
 package com.ecommerce.admin.service;
 
 import com.ecommerce.library.dto.AdminDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
 public interface AuthService {
-    String addNewAdmin(AdminDto adminDto,
-                       BindingResult result,
-                       Model model);
+    String addNewAdmin(AdminDto adminDto, BindingResult result, Model model);
 }
